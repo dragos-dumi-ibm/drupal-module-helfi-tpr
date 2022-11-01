09d6e0f4bf7058d9cbb88ac06e59846df47e0a4f
ovidiu <ovidiu.ardelean@ibm.com>
2022-11-01 14:46:11 +0200

    made name_synonyms field readonly and of type string_long

--- src/Entity/Service.php
+++ src/Entity/Service.php
@@ -150,11 +150,14 @@ class Service extends TprEntityBase {
   public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
     $fields = parent::baseFieldDefinitions($entity_type);
 
-    $fields['name_synonyms'] = BaseFieldDefinition::create('string')
+    $fields['name_synonyms'] = BaseFieldDefinition::create('string_long')
       ->setTranslatable(TRUE)
       ->setRevisionable(FALSE)
       ->setLabel(new TranslatableMarkup('Name synonyms'))
       ->setDescription(new TranslatableMarkup('Name synonyms for the service title.'))
+      ->setDisplayOptions('form', [
+        'type' => 'readonly_field_widget',
+      ])
       ->setDisplayConfigurable('form', TRUE)
       ->setDisplayConfigurable('view', TRUE);
 
